b331e6028d19ce1839bf50f12ab2b958a844c7be
Mathew-Samir <85489453+Mathew-Samir@users.noreply.github.com>
2024-07-25 16:21:32 +0300

    Update WeatherData.jsx

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -3,7 +3,7 @@ const WeatherData = ({ data }) => {
 return (
     <>
         {data.weather ?(
-            <div className="w-[90%] lg:w-[70%] bg-gray-300 shadow-lg rounded-lg m-auto relative px-6 top[10%]">
+            <div className="w-[100%] lg:w-[70%] bg-gray-300 shadow-lg rounded-lg m-auto relative px-6 top[10%]">
                 <div className="flex justify-between w-full">
                     <div className="w-1/2 my-4 mx-auto flex justify-between items-center">
                         <div className="flex flex-col items-center h-[80%]">
@@ -26,19 +26,19 @@ return (
                         </div>
                         {data.name !== undefined ? (
                             <div className="flex flex-col justify-evenly gap-y-2 my-4 mx-auto text-xs">
-                                <div className="flex justify-between gap-x-8">
+                                <div className="flex justify-between gap-x-2">
                                     <p>Feels Like</p>
                                     <p className='font-bold'>{data.main.feels_like.toFixed()}°C</p>
                                 </div>
-                                <div className="flex justify-between gap-x-8">
+                                <div className="flex justify-between gap-x-2">
                                     <p>Humidity</p>
                                     <p className='font-bold'>{data.main.humidity}%</p>
                                 </div>
-                                <div className="flex justify-between gap-x-8">
+                                <div className="flex justify-between gap-x-2">
                                     <p>wind</p>
                                     <p className='font-bold'>{data.wind.speed.toFixed()} KPH</p>
                                 </div>
-                                <div className="flex justify-between gap-x-8">
+                                <div className="flex justify-between gap-x-2">
                                     <p>Pressure</p>
                                     <p className='font-bold'>{data.main.pressure} hPa</p>
                                 </div>
