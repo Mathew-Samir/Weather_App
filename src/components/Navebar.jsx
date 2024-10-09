4b802df1c8af566585cce0f03f92d25f5f8c357a
Mathew Samir <85489453+Mathew-Samir@users.noreply.github.com>
2024-10-09 14:23:25 +0300

    Update Navebar.jsx

--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -7,11 +7,11 @@ return (
     <>
         <nav  className='bg-slate-200 py-4 w-[100%]'>
             <Link
-                className='mx-6 p-1 bg-slate-400 text-white rounded-lg shadow-md'
+                className='mx-1 p-1 bg-slate-400 text-white rounded-lg shadow-md'
                 to="/">Weather design 1
             </Link>
             <Link
-                className='mx-6 p-1 bg-slate-400 text-white rounded-lg shadow-md'
+                className='mx-1 p-1 bg-slate-400 text-white rounded-lg shadow-md'
                 to="/Weather2">Weather design 2
             </Link>
         </nav>
